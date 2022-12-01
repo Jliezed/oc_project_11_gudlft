a06513b46459e1c46cb01a40f9348c8b9993ae86
Jliezed <jliezed@gmail.com>
2022-12-01 11:40:07 +0100

    handle historic bookings

--- server.py
+++ server.py
@@ -54,10 +54,13 @@ def purchasePlaces():
         0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+    new_competition_seats = int(competition['numberOfPlaces']) - placesRequired
 
-    # Limit booking at 12 seats per competition
     new_club_points = int(club['points']) - placesRequired
+    try:
+        current_club_seats_competition = int(competition[club["name"]])
+    except KeyError:
+        current_club_seats_competition = 0
 
     error_messages = []
     if new_club_points < 0:
@@ -66,7 +69,22 @@ def purchasePlaces():
     if placesRequired > 12:
         error_message = "You can only book 12 seats maximum"
         error_messages.append(error_message)
+    if current_club_seats_competition >= 12:
+        error_message = "You already booked 12 seats for this competition"
+        error_messages.append(error_message)
     else:
+        with open('competitions.json', "w") as file:
+            for comp in competitions:
+                if comp["name"] == competition["name"]:
+                    comp["numberOfPlaces"] = str(new_competition_seats)
+                    # Save club seats for the competition
+                    comp[club["name"]] = str(placesRequired +
+                                             current_club_seats_competition)
+            data = {
+                "competitions": competitions,
+            }
+            json.dump(data, file)
+
         flash('Great-booking complete!')
 
     return render_template('welcome.html', club=club, competitions=competitions,
